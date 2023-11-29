64495f6a953bb9e9fe64858dce5a23e9d095ac1e
Dathison <david-hjemme@hotmail.com>
2023-11-29 18:00:22 +0100

    #3 Added initial attempt at doing YT playback.

--- SportsBot.py
+++ SportsBot.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 from adjustText import adjust_text
 import sqlite3
 import pytz
+import youtube_dl
 
 DB_NAME = "leaderboard.db"
 db_create = """
@@ -36,6 +37,46 @@ intents = discord.Intents.all() # or .all() if you ticked all, that is easier
 intents.members = True # If you ticked the SERVER MEMBERS INTENT
 description = '''The new and improved Gaudium you never knew you needed!'''
 bot = commands.Bot(command_prefix='g!', description=description, help_command=commands.DefaultHelpCommand(), intents=intents)
+
+#### DEFINING YT-DL DATA
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {'options': '-vn'}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data):
+        super().__init__(source)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+####
 
 class Leaderboard:
     _instance = None
@@ -1049,6 +1090,32 @@ async def stats(ctx, stat):
             embed.set_image(url="attachment://image.png")
             await ctx.send(file=file, embed=embed)
             plt.close()
+
+@bot.command(name='play', help='Play a song from YouTube')
+async def play(ctx, url):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+
+    channel = ctx.message.author.voice.channel
+
+    if not ctx.voice_client:
+        await channel.connect()
+    else:
+        await ctx.voice_client.move_to(channel)
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=bot.loop)
+        ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+    await ctx.send(f'Now playing: {player.title}')
+
+@bot.command(name='leave', help='Leave the voice channel')
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
 try:
     lb = Leaderboard()
